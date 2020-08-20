9656fb8736e1d9192e6d93e75343d37ebbcb44cf
Ayman Shabana <53878617+AymanShabana@users.noreply.github.com>
2020-08-20 23:04:52 +0200

    leaders and promotions db set up

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var promoRouter = require('./routes/promoRouter');
 
 const mongoose = require('mongoose');
 
-const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
